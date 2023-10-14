b58d29664baa920ea0f4ed7042c3d517693471b2
Evan <3965137+evanvolgas@users.noreply.github.com>
2023-10-14 19:02:44 -0400

    move data models to its own folder

--- data_models/analyses/missing_tickers.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-count(distinct b.ticker ) as ticker
-FROM {{ ref('stg_all_tickers') }} as a
-full outer join {{ ref('stg_all_tickers_historical') }} as b
-on a.ticker = b.ticker
-where a.ticker is null
