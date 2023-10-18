014af4a8194479e076b57c5a63aaafded62d43fc
Evan <3965137+evanvolgas@users.noreply.github.com>
2023-10-18 04:42:18 -0400

    dbt deps

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ elif [ "$1" = "sec_financials" ]; then
 elif [ "$1" = "sec_tickers" ]; then
     python scrapers/sec/sec_tickers.py
 elif [ "$1" = "run" ]; then
-    cd data_models && && dbt deps && dbt run
+    cd data_models && dbt deps && dbt run
 elif [ "$1" = "test" ]; then
     cd data_models && dbt deps && dbt test
 else
