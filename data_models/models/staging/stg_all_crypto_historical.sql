ae2003f69c0a99b1b77437d49cbd48f15a6cd7ae
Evan <3965137+evanvolgas@users.noreply.github.com>
2023-10-24 11:09:38 -0400

    add crypto to schema

---
+++ data_models/models/staging/stg_all_crypto_historical.sql
@@ -0,0 +1,15 @@
+with source_data as (
+SELECT
+    ticker,
+    open,
+    close,
+    volume_weighted_average_price,
+    volume,
+    transactions,
+    date,
+    EXTRACT(YEAR from date) as year,
+    EXTRACT(MONTH from date) as month,
+    EXTRACT(quarter from date) as quarter
+FROM raw.all_crypto_history
+)
+SELECT * FROM source_data
