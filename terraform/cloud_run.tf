8b23a92e20b34d983468362db5472be3f715363b
Evan <3965137+evanvolgas@users.noreply.github.com>
2023-10-26 14:12:21 -0400

    add terraform, pt 2

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -161,7 +161,7 @@ resource "google_cloud_run_v2_job" "alltickersdetail" {
         resources {
           limits = {
             cpu    = 1
-            memory = "2Gi"
+            memory = "4Gi"
           }
         }
         env {
@@ -247,6 +247,103 @@ resource "google_cloud_run_v2_job" "alltickersfinancials" {
               version = "latest"
             }
           }
+        }
+      }
+      timeout         = "900s"
+      service_account = "admin-509@new-life-400922.iam.gserviceaccount.com"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      launch_stage,
+    ]
+  }
+}
+
+
+resource "google_cloud_run_v2_job" "dbtrun" {
+  name     = "tf-dbtrun"
+  location = "us-west1"
+
+  template {
+    template {
+      volumes {
+        name = "secret-volume"
+        secret {
+          secret       = "dbt_credentials"
+          default_mode = 292 # 0444
+          items {
+            version = "latest"
+            path    = "dbt_credentials"
+            mode    = 256 # 0400
+          }
+        }
+      }
+      containers {
+        image = "us-west1-docker.pkg.dev/new-life-400922/arapbi/arapbi:latest"
+        volume_mounts {
+          name       = "secret-volume"
+          mount_path = "/secrets"
+        }
+        args = ["run"]
+        resources {
+          limits = {
+            cpu    = 1
+            memory = "2Gi"
+          }
+        }
+        env {
+          name  = "GOOGLE_APPLICATION_CREDENTIALS"
+          value = "/secrets/dbt_credentials"
+        }
+      }
+      timeout         = "900s"
+      service_account = "admin-509@new-life-400922.iam.gserviceaccount.com"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      launch_stage,
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_job" "dbttest" {
+  name     = "tf-dbttest"
+  location = "us-west1"
+
+  template {
+    template {
+      volumes {
+        name = "secret-volume"
+        secret {
+          secret       = "dbt_credentials"
+          default_mode = 292 # 0444
+          items {
+            version = "latest"
+            path    = "dbt_credentials"
+            mode    = 256 # 0400
+          }
+        }
+      }
+      containers {
+        image = "us-west1-docker.pkg.dev/new-life-400922/arapbi/arapbi:latest"
+        volume_mounts {
+          name       = "secret-volume"
+          mount_path = "/secrets"
+        }
+        args = ["test"]
+        resources {
+          limits = {
+            cpu    = 1
+            memory = "2Gi"
+          }
+        }
+        env {
+          name  = "GOOGLE_APPLICATION_CREDENTIALS"
+          value = "/secrets/dbt_credentials"
         }
       }
       timeout         = "900s"
